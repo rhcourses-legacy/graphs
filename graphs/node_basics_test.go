2000bedba5ad76a17c4983d617fc168dddb3f40f
Reiner Hüchting <dev@reinerhuechting.de>
2023-07-02 21:07:13 +0200

    Simplify basic tests.

--- graphs/node_basics_test.go
+++ graphs/node_basics_test.go
@@ -6,13 +6,12 @@ import "testing"
 // It creates a new node and checks that the label is correct
 // and that there are no neighbours.
 func TestNode_New(t *testing.T) {
+	gt := GraphTester{t}
+
 	n := NewNode("A")
-	if n.Label != "A" {
-		t.Errorf("Expected label A, got %s", n.Label)
-	}
-	if n.NeighbourCount() != 0 {
-		t.Errorf("Expected 0 neighbours, got %d", n.NeighbourCount())
-	}
+
+	gt.assertNodeLabelEquals(n, "A")
+	gt.assertNeighbourCountEquals(n, 0)
 }
 
 // TestNode_String tests the String method.
@@ -27,12 +26,12 @@ func TestNode_String(t *testing.T) {
 // TestNode_NeighbourCount tests the NeighbourCount method.
 // It creates a new node, adds neighbours, and checks that the number of neighbours is correct.
 func TestNode_NeighbourCount(t *testing.T) {
+	gt := GraphTester{t}
+
 	n := NewNode("A")
 	n.AddNeighbour("B")
 	n.AddNeighbour("C")
-	if n.NeighbourCount() != 2 {
-		t.Errorf("Expected 2 neighbours, got %d", n.NeighbourCount())
-	}
+	gt.assertNeighbourCountEquals(n, 2)
 }
 
 // TestNode_GetNeighbour tests the GetNeighbour method.
@@ -40,18 +39,14 @@ func TestNode_NeighbourCount(t *testing.T) {
 // - the neighbours are correct
 // - for a non-existent neighbour, nil is returned
 func TestNode_GetNeighbour(t *testing.T) {
+	gt := GraphTester{t}
 	n := NewNode("A")
 	n.Edges = append(n.Edges, *NewEdge("B"))
 	n.Edges = append(n.Edges, *NewEdge("C"))
-	if n.GetNeighbour("B").Label != "B" {
-		t.Errorf("Expected neighbour B, got %v", n.GetNeighbour("B"))
-	}
-	if n.GetNeighbour("C").Label != "C" {
-		t.Errorf("Expected neighbour C, got %v", n.GetNeighbour("C"))
-	}
-	if n.GetNeighbour("D") != nil {
-		t.Errorf("Expected nil, got %v", n.GetNeighbour("D"))
-	}
+
+	gt.assertNeighbourHasCorrectLabel(n, "B")
+	gt.assertNeighbourHasCorrectLabel(n, "C")
+	gt.assertNeighbourIsNode(n, nil, "D")
 }
 
 // TestNode_AddNeighbour tests the AddNeighbour method.
--- graphs/testhelpers_test.go
+++ graphs/testhelpers_test.go
@@ -8,8 +8,55 @@ type GraphTester struct {
 	t *testing.T
 }
 
+// assertNodeLabelEquals
+func (gt *GraphTester) assertNodeLabelEquals(n *Node, expectedlabel string) {
+	if n == nil {
+		gt.t.Errorf("Expected label %s, but node is nil", expectedlabel)
+		return
+	}
+	if n.Label != expectedlabel {
+		gt.t.Errorf("Expected label %s, but got %s", expectedlabel, n.Label)
+	}
+}
+
+// assertNeighbourCountEquals expects a node and an int.
+// Checks if the node has that number of neighbours.
+// Produces a test failure if not.
+func (gt *GraphTester) assertNeighbourCountEquals(n *Node, expectedCount int) {
+	actualCount := n.NeighbourCount()
+	if actualCount != expectedCount {
+		gt.t.Errorf("Expected %d neighbours, got %d", expectedCount, actualCount)
+	}
+}
+
+// assertNeighbourHasCorrectLabel expects a node and a label string.
+// Checks if the node's GetNeighbour method returns a node for that label
+// and that the node's label is correct.
+// Produces a test failure if not.
+func (gt *GraphTester) assertNeighbourHasCorrectLabel(n *Node, label string) {
+	neighbour := n.GetNeighbour(label)
+	if neighbour == nil {
+		gt.t.Errorf("Expected neighbour %s, got nil", label)
+		return
+	}
+	if neighbour.Label != label {
+		gt.t.Errorf("Expected neighbour %s, got %s", label, neighbour.Label)
+	}
+}
+
+// assertNeighbourIsNode expects two nodes n, m, and a label string.
+// Checks if the node returned by n.GetNeighbour for that label is n.
+// Produces a test failure if not.
+func (gt *GraphTester) assertNeighbourIsNode(n, m *Node, label string) {
+	neighbour := n.GetNeighbour(label)
+	if neighbour != m {
+		gt.t.Errorf("Expected neighbour %v, got %v", m, neighbour)
+	}
+}
+
 // assertNodesHaveLabels expects a list of nodes and some strings.
 // Checks that the nodes have the given strings as labels (in that order).
+// None of the nodes may be nil!
 // Produces a test failure if not.
 func (gt *GraphTester) assertNodesHaveLabels(nodes []*Node, expectedlabels ...string) {
 	actuallabels := []string{}
@@ -28,7 +75,7 @@ func (gt *GraphTester) assertNodesHaveLabels(nodes []*Node, expectedlabels ...st
 	}
 }
 
-// assertReachable_maxdist expects a Node, a label and a maximum distance md.
+// assertReachable_maxdist expects a node, a label and a maximum distance md.
 // Checks if a node with the givel label can be reached in at most md steps.
 // If not, produces a test failure.
 func (gt *GraphTester) assertReachable_maxdist(n *Node, label string, md int) {
@@ -37,7 +84,7 @@ func (gt *GraphTester) assertReachable_maxdist(n *Node, label string, md int) {
 	}
 }
 
-// assertReachable_maxdist expects a Node, a label and a maximum distance md.
+// assertReachable_maxdist expects a node, a label and a maximum distance md.
 // Checks if a node with the givel label cannot be reached in at most md steps.
 // If it can be reached, produces a test failure.
 func (gt *GraphTester) assertUnreachable_maxdist(n *Node, label string, md int) {
